9a6ab8a17724596c833907621e5197ccbd4d4bbe
Ralph Otowo <ralph@otowo.me>
2021-07-18 21:32:26 +0100

    Print Tree URL

--- build.sh
+++ build.sh
@@ -6,11 +6,14 @@ docker=$(which docker)
 kubectl=$(which kubectl)
 minikube=$(which minikube)
 
-minikube start
-minikube addons enable ingress
+$minikube start
+$minikube addons enable ingress
 
 eval $(minikube docker-env)
-docker build -t ecosia/sre-asst:v1.0 .
 
-kubectl apply -f k8s/deployment.yml -f k8s/service.yml
+$docker build -t ecosia/sre-asst:v1.0 .
+$kubectl apply -f k8s/deployment.yml -f k8s/service.yml
 
+URL=$($minikube service --url ecosia-svc)
+
+echo "Visit: ${URL}/tree"
