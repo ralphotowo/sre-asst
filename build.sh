623f4b6879d4868f8534af83b651012e04aa4117
Ralph Otowo <ralph@otowo.me>
2021-07-18 22:12:46 +0100

    Added Ingress

--- build.sh
+++ build.sh
@@ -12,8 +12,25 @@ $minikube addons enable ingress
 eval $(minikube docker-env)
 
 $docker build -t ecosia/sre-asst:v1.0 .
-$kubectl apply -f k8s/deployment.yml -f k8s/service.yml
+$kubectl apply -f k8s/deployment.yml -f k8s/service.yml -f k8s/ingress.yml
+
+printf "\n\n"
+
+sleep 3; echo "Execution complete!"
+sleep 2; echo "The deployment and service have been created and is accessible at the following URL."
+
+printf "\n\n"
 
 URL=$($minikube service --url ecosia-svc)
-
 echo "Visit: ${URL}/tree"
+
+sleep 4; printf "\n\n"
+
+echo "The ingress rule has also been created. The IP address returned below should be added to /etc/hosts with the value set to local.ecosia.org"
+
+$kubectl get ingress
+
+printf "\n\n"
+
+sleep 3; echo "Once the local DNS record is created, the JSON output will be visible at local.ecosia.org/tree"
+sleep 1; echo "Thank you for taking the time to review!"
