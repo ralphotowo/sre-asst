ce86f8c262068e3224b026ce425bfde49ea5bdac
Ralph Otowo <ralph@otowo.me>
2021-07-18 22:18:02 +0100

    Added sleep for ingress IP allocation

--- build.sh
+++ build.sh
@@ -27,8 +27,9 @@ echo "Visit: ${URL}/tree"
 sleep 4; printf "\n\n"
 
 echo "The ingress rule has also been created. The IP address returned below should be added to /etc/hosts with the value set to local.ecosia.org"
+echo "It may take a few moments for the IP address to be returned."
 
-$kubectl get ingress
+sleep 30; $kubectl get ingress
 
 printf "\n\n"
 
